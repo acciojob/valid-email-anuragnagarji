b88627a937f9b3e9e79346808fd81b89e69b6e3f
anuragnagarji <142933378+anuragnagarji@users.noreply.github.com>
2023-12-27 22:53:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function validEmail(str) {
-  //your JS code here.
+	let arr=str.split(/@|.);
+ if(str.includes(" ")||str.length<5||str.indexOf('@')==0||(str.indexOf('@')+1)>=str.indexOf(".")||str.indexOf(".")==(str.length-1)){
+	 return false;
+ }
+	
+		return true;
+	
+
 }
 
+
 // Do not change the code below.
-const str = prompt("Enter an email address.");
+const str = prompt("Enter an email addpt");
 alert(validEmail(str));
+
